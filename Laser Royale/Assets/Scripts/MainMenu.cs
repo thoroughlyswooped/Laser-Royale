6245bea23810bc4ef8cfca9adb1346c62ff08443
nct31 <100656878+nct31@users.noreply.github.com>
2022-03-03 19:51:06 -0800

    Homepage and Pause Menu

---
+++ Laser Royale/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour{
+    
+    public void Play(){
+        SceneManager.LoadScene(1);
+    }
+
+
+    //Mario's Script
+    public void Quit()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+
+    }
+}
