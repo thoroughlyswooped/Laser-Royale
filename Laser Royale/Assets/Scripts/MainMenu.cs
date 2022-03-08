d6d62dc67456894228f0e22cdd499f36a7741443
Joshua Alvarado <47153947+JoshAlvarado@users.noreply.github.com>
2022-03-07 16:20:47 -0800

    Added Levels & start level 1

--- Laser Royale/Assets/Scripts/MainMenu.cs
+++ Laser Royale/Assets/Scripts/MainMenu.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour{
     
     public void Play(){
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene("Assets/Scenes/Level1.unity");
     }
 
 
